5a76482c63968daff8aaab03426488b11eab0e8d
M1xSerM4s <tokarm214@gmail.com>
2021-11-08 12:24:41 +0300

    title-page v2.0 no adaptive

--- title-page/app/js/main.js
+++ title-page/app/js/main.js
@@ -1,1 +1,32 @@
-$(function () {});
+$(function () {
+  $(".examples__content").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    draggable: false,
+    nextArrow:
+      '<img class="arrow arrow--right" src="images/icons/arrow-right.png" width="64" height="64" alt="arrow right icon">',
+    prevArrow:
+      '<img class="arrow arrow--left" src="images/icons/arrow-left.png" width="64" height="64" alt="arrow left icon">',
+    // responsive: [
+    //   {
+    //     breakpoint: 768,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: "40px",
+    //       slidesToShow: 3,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 640,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: "40px",
+    //       slidesToShow: 1,
+    //     },
+    //   },
+    // ],
+  });
+});
--- title-page/gulpfile.js
+++ title-page/gulpfile.js
@@ -1,69 +1,71 @@
-const { src, dest, watch, parallel, series } = require('gulp');
+const { src, dest, watch, parallel, series } = require("gulp");
 
-const scss = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
-const uglify = require('gulp-uglify-es').default;
-const autoprefixer = require('gulp-autoprefixer');
-const image = require('gulp-image');
-const del = require('del');
-
+const scss = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const browserSync = require("browser-sync").create();
+const uglify = require("gulp-uglify-es").default;
+const autoprefixer = require("gulp-autoprefixer");
+const image = require("gulp-image");
+const del = require("del");
 
 function browsersync() {
-    browserSync.init({
-        server: {
-            baseDir: 'app/'
-        },
-    });
+  browserSync.init({
+    server: {
+      baseDir: "app/",
+    },
+  });
 }
 
 function cleanDist() {
-    return del('dist')
+  return del("dist");
 }
 
-function images(){
-    return src('app/images/**/*')
-        .pipe(image())
-        .pipe(dest('dist/images'))
+function images() {
+  return src("app/images/**/*").pipe(image()).pipe(dest("dist/images"));
 }
 
-function styles(){
-    return src('app/scss/style.scss')
-        .pipe(scss({outputStyle: 'compressed'}))
-        .pipe(concat('style.min.css'))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 10 version'],
-            grid: true
-        }))
-        .pipe(dest('app/css'))
-        .pipe(browserSync.stream())
+function styles() {
+  return src("app/scss/style.scss")
+    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(concat("style.min.css"))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 10 version"],
+        grid: true,
+      })
+    )
+    .pipe(dest("app/css"))
+    .pipe(browserSync.stream());
 }
 
-function build(){
-    return src([
-        'app/css/style.min.css',
-        'app/fonts/**/*',
-        'app/js/main.min.js',
-        'app/*.html'
-    ], {base: 'app'})
-        .pipe(dest('dist'))
+function build() {
+  return src(
+    [
+      "app/css/style.min.css",
+      "app/fonts/**/*",
+      "app/js/main.min.js",
+      "app/*.html",
+    ],
+    { base: "app" }
+  ).pipe(dest("dist"));
 }
 
 function watching() {
-    watch(['app/scss/**/*.scss'], styles);
-    watch(['app/js/**/*.js','!app/js/main.min.js'], scripts);
-    watch(['app/*.html']).on('change', browserSync.reload);
+  watch(["app/scss/**/*.scss"], styles);
+  watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
+  watch(["app/*.html"]).on("change", browserSync.reload);
 }
 
-function scripts(){
-    return src([
-        'node_modules/jquery/dist/jquery.js',
-        'app/js/main.js'
-    ])
-        .pipe(concat('main.min.js'))
-        .pipe(uglify())
-        .pipe(dest('app/js'))
-        .pipe(browserSync.stream())
+function scripts() {
+  return src([
+    "node_modules/jquery/dist/jquery.js",
+    "node_modules/slick-carousel/slick/slick.js",
+    "app/js/main.js",
+  ])
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(dest("app/js"))
+    .pipe(browserSync.stream());
 }
 
 exports.styles = styles;
