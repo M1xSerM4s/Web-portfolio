8ec4cd0855074f831e2ca1951453fac146724d7b
M1xSerM4s <tokarm214@gmail.com>
2021-11-10 12:38:20 +0300

    title-page adaptive added

--- title-page/app/js/main.js
+++ title-page/app/js/main.js
@@ -8,25 +8,13 @@ $(function () {
       '<img class="arrow arrow--right" src="images/icons/arrow-right.png" width="64" height="64" alt="arrow right icon">',
     prevArrow:
       '<img class="arrow arrow--left" src="images/icons/arrow-left.png" width="64" height="64" alt="arrow left icon">',
-    // responsive: [
-    //   {
-    //     breakpoint: 768,
-    //     settings: {
-    //       arrows: false,
-    //       centerMode: true,
-    //       centerPadding: "40px",
-    //       slidesToShow: 3,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 640,
-    //     settings: {
-    //       arrows: false,
-    //       centerMode: true,
-    //       centerPadding: "40px",
-    //       slidesToShow: 1,
-    //     },
-    //   },
-    // ],
+    responsive: [
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 });
